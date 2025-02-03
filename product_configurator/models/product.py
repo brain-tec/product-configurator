pledra/odoo-product-configurator

[FIX] Formatting prices according to language in website

product_configurator/models/product.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 
+from odoo.tools.misc import formatLang
 from odoo.exceptions import ValidationError
 from odoo import models, fields, api, tools, _
 from lxml import etree
@@ -108,13 +109,25 @@ class ProductTemplate(models.Model):
                 })
         return adjacent_steps
 
+    def formatPrices(self, prices={}):
+        prices['taxes'] = formatLang(self.env, prices['taxes'], monetary=True)
+        prices['total'] = formatLang(self.env, prices['total'], monetary=True)
+        prices['vals'] = [
+            (v[0], v[1], formatLang(self.env, v[2], monetary=True))
+            for v in prices['vals']
+        ]
+        return prices
+
     @api.multi
-    def get_cfg_price(self, value_ids, custom_values={}, pricelist_id=None):
+    def get_cfg_price(self, value_ids, custom_values={},
+                      pricelist_id=None, formatLang=False):
         """ Computes the price of the configured product based on the configuration
-            passed in the config_vals dictionary
+            passed in via value_ids and custom_values
 
         :param value_ids: list of attribute value_ids
         :param custom_values: dictionary of custom attribute values
+        :param pricelist_id: id of pricelist to use for price computation
+        :param formatLang: boolean for formatting price dictionary
         :returns: dictionary of prices per attribute and total price"""
         self.ensure_one()
         if not pricelist_id:
@@ -173,6 +186,8 @@ class ProductTemplate(models.Model):
             taxes = total_included - product_prices['total_excluded']
             prices['taxes'] += taxes
             prices['total'] += total_included
+        if formatLang:
+            return self.formatPrices(prices)
         return prices
 
     @api.multi

brain-tec/product-configurator

product_configurator/models/product.py
@@ -3,6 +3,7 @@
 from lxml import etree
 from ast import literal_eval
 
+from openerp.tools.misc import formatLang
 from openerp.exceptions import ValidationError
 from openerp import models, fields, api, tools, _
 from openerp.addons import decimal_precision as dp
@@ -110,13 +111,25 @@ class ProductTemplate(models.Model):
                 })
         return adjacent_steps
 
+    def formatPrices(self, prices={}):
+        prices['taxes'] = formatLang(self.env, prices['taxes'], monetary=True)
+        prices['total'] = formatLang(self.env, prices['total'], monetary=True)
+        prices['vals'] = [
+            (v[0], v[1], formatLang(self.env, v[2], monetary=True))
+            for v in prices['vals']
+        ]
+        return prices
+
     @api.multi
-    def get_cfg_price(self, value_ids, custom_values={}, pricelist_id=None):
+    def get_cfg_price(self, value_ids, custom_values={},
+                      pricelist_id=None, formatLang=False):
         """ Computes the price of the configured product based on the configuration
-            passed in the config_vals dictionary
+            passed in via value_ids and custom_values
 
         :param value_ids: list of attribute value_ids
         :param custom_values: dictionary of custom attribute values
+        :param pricelist_id: id of pricelist to use for price computation
+        :param formatLang: boolean for formatting price dictionary
         :returns: dictionary of prices per attribute and total price"""
         self.ensure_one()
         if not pricelist_id:
@@ -175,6 +188,8 @@ class ProductTemplate(models.Model):
             taxes = total_included - product_prices['total_excluded']
             prices['taxes'] += taxes
             prices['total'] += total_included
+        if formatLang:
+            return self.formatPrices(prices)
         return prices
 
     @api.multi